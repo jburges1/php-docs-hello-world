0929e8e00ccff83bea5291e8ad3acbb0c7a10666
jburges1 <122888968+jburges1@users.noreply.github.com>
2023-01-17 15:04:48 +0100

    php Anwendnung

--- index.php
+++ index.php
@@ -1,3 +1,15 @@
-<?php
 
-echo "Hello World!";
+<!-- saved from url=(0063)http://fbwallinone.th-brandenburg.de/~burges/PrimzahlFinder.php -->
+<html><head><meta http-equiv="Content-Type" content="text/html; charset=UTF-8"></head><body>
+<h1> Kleine Programmieraufgabe Primzahlenfinder- Jakob Burges  </h1><h1>
+
+<form action="http://fbwallinone.th-brandenburg.de/~burges/PrimzahlFinder.php" method="post">
+Von: <input type="int" name="von"><br>
+Bis: <input type="int" name="bis"><br>
+<input type="submit">
+</form>
+
+ 
+
+
+</h1></body></html>
